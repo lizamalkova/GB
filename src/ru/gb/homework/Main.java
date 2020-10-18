02e60b23ebdb5b18eff083a61249850186b1caa4
lizamalkova <71965562+lizamalkova@users.noreply.github.com>
2020-10-18 15:18:03 +0200

    Update Main.java

--- src/ru/gb/homework/Main.java
+++ src/ru/gb/homework/Main.java
@@ -1,30 +1,45 @@
-public class Cat
-{
-    private String name;
-    private  int appetite;
-    private boolean full = false;
+package ru.gb.homework;
 
-    public  Cat(String name, int appetite)
+public class Main {
+
+    public static void main(String[] args)
     {
-        this.name = name;
-        this.appetite = appetite;
+        Cat cat1 = new Cat("Дуся", 8);
+        Cat cat2 = new Cat("Тася", 10);
+        Cat cat3 = new Cat("Мася", 12);
+        Plate plate1 = new Plate(10);
+        Plate plate2 = new Plate(25);
+        Plate plate3 = new Plate(7);
+        Plate plate4 = new Plate(50);
+
+        plate3.info();
+        cat1.eat(plate3);
+        plate3.info();
+
+        plate2.info();
+        cat2.eat(plate2);
+        cat3.eat(plate2);
+        plate2.info();
+        cat1.eat(plate2);
+        plate2.info();
+
+        Cat[] cats = { cat1, cat2, cat3 };
+
+        plate4.info();
+        catsAreEating(cats,plate4);
+        plate4.info();
+
+        plate1.info();
+        plate1.putFood(7);
+        plate1.info();
     }
 
-    public void eat(Plate plate)
+    static void catsAreEating(Cat[] cats, Plate plate)
     {
-        if ( plate.getFood() >= appetite )
+        for ( int i = 0; i < cats.length; i++)
         {
-            plate.decreaseFood(appetite);
-            full = true;
-            System.out.println("Коша " + this.name + " сыта!");
-
-        }
-        else
-        {
-            System.out.println("Аппетит у кошки больше, чем еды в миске!");
-            System.out.println("Коша " + this.name + " голодная!");
+            cats[i].eat(plate);
         }
     }
 
-
-
+}
